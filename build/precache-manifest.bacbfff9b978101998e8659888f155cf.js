5ae27f1ec83ac0f53d2d043b2e8e67c851e50983
Daedalus <kyn.mak@gmail.com>
2019-06-23 17:11:19 -0700

    6/23 fix

---
+++ build/precache-manifest.bacbfff9b978101998e8659888f155cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8f0ea348cd2e4f788f6",
+    "url": "/static/css/main.a1b90e01.chunk.css"
+  },
+  {
+    "revision": "b8f0ea348cd2e4f788f6",
+    "url": "/static/js/main.b8f0ea34.chunk.js"
+  },
+  {
+    "revision": "b63e9ff15fa74d024a07",
+    "url": "/static/css/1.3c750adb.chunk.css"
+  },
+  {
+    "revision": "b63e9ff15fa74d024a07",
+    "url": "/static/js/1.b63e9ff1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e062a2427c75cbb452ff0ef1abb067ff",
+    "url": "/index.html"
+  }
+];
