4419f672b05e253a3c8cc24b76bc5ab8df447b70
Daedalus <kyn.mak@gmail.com>
2019-06-27 11:35:01 -0700

    Updates

--- precache-manifest.6c95e460f23b749329393f182efdabe8.js
+++ precache-manifest.6c95e460f23b749329393f182efdabe8.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "5c16526056c0ad54a81c",
-    "url": "/static/css/main.47828b54.chunk.css"
+    "revision": "8920c448364a37606117",
+    "url": "/static/css/main.44024f5d.chunk.css"
   },
   {
-    "revision": "5c16526056c0ad54a81c",
-    "url": "/static/js/main.5c165260.chunk.js"
+    "revision": "8920c448364a37606117",
+    "url": "/static/js/main.8920c448.chunk.js"
   },
   {
     "revision": "b63e9ff15fa74d024a07",
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "5e061af014ec19f2d2f7c41da1b7972f",
+    "revision": "e3e35fc90d7b21f7ab61bb8fe7ba51ff",
     "url": "/index.html"
   }
 ];
